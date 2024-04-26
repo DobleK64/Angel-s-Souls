a74ec4ce531ceb4f9023612280d79750d6362b02
LordRivero <carlosrive99@gmail.com>
2024-04-26 13:41:51 +0200

    Pantalla GameOver con sonido, Death Zone y Menu

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement pm = collision.GetComponent<PlayerMovement>();
+        if (pm)
+        {
+            pm.resetPersonaje();
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
